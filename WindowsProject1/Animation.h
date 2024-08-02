365bdce09e5067bc559eebd6101d3bdf4c100c78
alphaom33 <alphaom34@gmail.com>
2024-08-02 17:30:45 -0500

    began adding animation

--- WindowsProject1/Animation.h
+++ WindowsProject1/Animation.h
@@ -1,7 +1,21 @@
+#include "IDrawable.h"
 #include "d2d1.h"
 #pragma once
-class Animation {
+class AnimatedSprite : IDrawable {
 public:
-	ID2D1Bitmap* frames[10];
+	AnimatedSprite(Vector2* position, Vector2* scale, ID2D1Bitmap* frames[], double speed);
+	void Play();
+
+	Vector2* GetPosition();
+	Vector2* GetScale();
+	ID2D1Bitmap** GetBitmap();
+
+private:
+	ID2D1Bitmap** frames;
 	double speed;
+
+	ID2D1Bitmap* current;
+
+	Vector2* position;
+	Vector2* scale;
 };
--- WindowsProject1/AnimationFrame.h
+++
@@ -1,8 +0,0 @@
-#include "d2d1.h"
-#pragma once
-class AnimationFrame
-{
-public:
-	ID2D1Bitmap* frame;
-};
-
